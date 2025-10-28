ef2ebf98dee22af60f7074d2808843d00a3988d8
Ibrar Khalid Farooqi <ibrarkhalidhng@gmail.com>
2025-10-28 10:02:48 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/irsolutions/location_based_social_platform/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.irsolutions.location_based_social_platform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:location_based_social_platform/src/auth/splash_screen.dart';
+
+void main() {
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(360, 640),
+      builder: (context, child) => const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(title: 'Flutter Demo', home: SplashScreen());
+  }
+}
